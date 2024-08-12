d5bca92845aa442dced31572ef7dea60b52d0157
Mario Krstevski <mariokrstevski@hotmail.com>
2024-08-12 14:58:09 +0200

    step8 content prep

--- src/app/listing/new/steps/Step8.tsx
+++ src/app/listing/new/steps/Step8.tsx
@@ -1,46 +1,11 @@
-const propertyCategory = [
-  "resitendial",
-  "commercial",
-  "land",
-  "other",
-];
-
-const propertyType = {
-  residential: [
-    "apartment",
-    "studio flat",
-    "maisonette",
-    "detached house",
-    "villa",
-    "loft",
-    "bungalow",
-    "building",
-    "apartment complex",
-    "farm",
-    "houseboat",
-    "other categories",
-  ],
-  commercial: [
-    "office",
-    "store",
-    "warehouse",
-    "industrial space",
-    "craft space",
-    "hotel",
-    "business building",
-    "showroom",
-    "other categories",
-  ],
-  land: ["land plot", "parcel", "island", "other categories"],
-  other: [
-    "garage",
-    "business",
-    "prefabricated",
-    "detachable",
-    "air",
-    "other categories",
-  ],
-};
 export default function Step8() {
-  return <div>Step1 works</div>;
+  return (
+    <div>
+      Step8 works
+      <div>date range for publishing</div>
+      <div>proceed to publish</div>
+      <div>payment for special features</div>
+      <div>terms and conditions?</div>
+    </div>
+  );
 }
