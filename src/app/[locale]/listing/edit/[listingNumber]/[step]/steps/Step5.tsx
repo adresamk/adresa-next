ad727bcb7610ae1bc4920f4f06bbcc3ac92852d0
Mario Krstevski <mariokrstevski@hotmail.com>
2024-12-09 09:11:18 +0100

    Step5 done

--- src/app/[locale]/listing/edit/[listingNumber]/[step]/steps/Step5.tsx
+++ src/app/[locale]/listing/edit/[listingNumber]/[step]/steps/Step5.tsx
@@ -7,104 +7,102 @@ import { Textarea } from "@/components/ui/textarea";
 import { useState } from "react";
 import { Listing } from "@prisma/client";
 import { Input } from "@/components/ui/input";
+import { useTranslations } from "next-intl";
 
-// Opis Description
 export default function Step5({ listing }: { listing: Listing }) {
-  const [description, setDescription] = useState(listing.description || "");
+  const t = useTranslations("listing.new.progress.steps.description");
+
+  const [engDescription, setEngDescription] = useState(
+    listing.description || "",
+  );
   const [mkdDescription, setMkdDescription] = useState(
     listing.mkdDescription || "",
   );
   const [albDescription, setAlbDescription] = useState(
     listing.albDescription || "",
   );
+
   return (
     <div className="p-2">
       <input type="string" className="hidden" defaultValue="5" name="step" />
 
-      <h2 className="text-lg">Description</h2>
+      <h2 className="text-lg">{t("title")}</h2>
       <Separator className="my-2 mt-4" />
 
       <div className="flex flex-col gap-2">
-        <Label htmlFor="title">Title</Label>
+        <Label htmlFor="engTitle">{t("englishTitle")}</Label>
+        <Input
+          name="engTitle"
+          className="pb-2"
+          id="engTitle"
+          defaultValue={listing.title || ""}
+          maxLength={112}
+          placeholder={t("englishTitlePlaceholder")}
+        />
 
-        <div className="">
-          <Input
-            name="title"
-            className="pb-2"
-            id="title"
-            defaultValue={listing.title || ""}
-            maxLength={112}
-          />
+        <Label htmlFor="engDescription">{t("englishDescription")}</Label>
+        <Textarea
+          name="engDescription"
+          className="pb-2"
+          rows={10}
+          id="engDescription"
+          value={engDescription}
+          maxLength={5000}
+          placeholder={t("englishDescriptionPlaceholder")}
+          onChange={(e) => setEngDescription(e.target.value)}
+        />
+        <div className="mt-1 text-sm text-slate-400">
+          {engDescription.length}/5000 {t("characters")}
         </div>
+        <Separator className="my-2 mt-4" />
+        <Label htmlFor="mkdTitle">{t("macedonianTitle")}</Label>
+        <Input
+          name="mkdTitle"
+          className="pb-2"
+          id="mkdTitle"
+          defaultValue={listing.mkdTitle || ""}
+          maxLength={112}
+          placeholder={t("macedonianTitlePlaceholder")}
+        />
 
-        <Label htmlFor="description">
-          Detailed english description for the property
-        </Label>
-
-        <div className="">
-          <Textarea
-            name="description"
-            className="pb-2"
-            rows={10}
-            id="description"
-            value={description}
-            maxLength={5000}
-            onChange={(e) => {
-              setDescription(e.target.value);
-            }}
-          />
-          <div className="mt-1 text-sm text-slate-400">
-            {description.length}/5000 characters
-          </div>
-          <Button type="button" className="mt-3">
-            Translate to
-          </Button>
+        <Label htmlFor="mkdDescription">{t("macedonianDescription")}</Label>
+        <Textarea
+          name="mkdDescription"
+          className="pb-2"
+          rows={10}
+          id="mkdDescription"
+          value={mkdDescription}
+          maxLength={5000}
+          placeholder={t("macedonianDescriptionPlaceholder")}
+          onChange={(e) => setMkdDescription(e.target.value)}
+        />
+        <div className="mt-1 text-sm text-slate-400">
+          {mkdDescription.length}/5000 {t("characters")}
         </div>
-      </div>
+        <Separator className="my-2 mt-4" />
+        <Label htmlFor="albTitle">{t("albanianTitle")}</Label>
+        <Input
+          name="albTitle"
+          className="pb-2"
+          id="albTitle"
+          defaultValue={listing.albTitle || ""}
+          maxLength={112}
+          placeholder={t("albanianTitlePlaceholder")}
+        />
 
-      <div className="mt-3 flex flex-col gap-2">
-        <div className="flex gap-3">
-          <Label htmlFor="mkdDescription">Macedonian Translation </Label>
-        </div>
-
-        <div className="">
-          <Textarea
-            className="pb-2"
-            rows={10}
-            id="mkdDescription"
-            name="mkdDescription"
-            value={mkdDescription}
-            maxLength={5000}
-            onChange={(e) => {
-              setMkdDescription(e.target.value);
-            }}
-          />
-          <div className="mt-1 text-sm text-slate-400">
-            {mkdDescription.length}/5000 characters
-          </div>
-        </div>
-      </div>
-
-      <div className="mt-3 flex flex-col gap-2">
-        <div className="flex gap-3">
-          <Label htmlFor="albDescription">Albanian Translation </Label>
-        </div>
-
-        <div className="">
-          <Textarea
-            className="pb-2"
-            rows={10}
-            maxLength={5000}
-            id="albDescription"
-            name="albDescription"
-            value={albDescription}
-            onChange={(e) => {
-              setAlbDescription(e.target.value);
-            }}
-          />
-          <div className="mt-1 text-sm text-slate-400">
-            {albDescription.length}/5000 characters
-          </div>
+        <Label htmlFor="albDescription">{t("albanianDescription")}</Label>
+        <Textarea
+          name="albDescription"
+          className="pb-2"
+          rows={10}
+          id="albDescription"
+          value={albDescription}
+          maxLength={5000}
+          placeholder={t("albanianDescriptionPlaceholder")}
+          onChange={(e) => setAlbDescription(e.target.value)}
+        />
+        <div className="mt-1 text-sm text-slate-400">
+          {albDescription.length}/5000 {t("characters")}
         </div>
       </div>
     </div>
